d7a94ed7b38aa4b2705fa17fc03b4f804144c36a
satishphogat <satish.phogat@kelltontech.com>
2020-02-12 18:40:46 +0530

    refactor: ui: change password screen.

--- ChangePassword.js
+++ ChangePassword.js
@@ -12,10 +12,19 @@ import { TextField } from 'react-native-material-textfield';
 
 export default class ChangePassword extends Component {
 
+  state  = {
+    newPassword: '',
+    confirmPassword: ''
+  }
+ 
   render() {
     return (
-      <View style={style.container}>
-       
+      <View style = {style.container}>
+      <TextField style={style.textInput} label="New Password" title="Enter new password here" tintColor='gray'></TextField>
+      <TextField style={style.textInput} label="Confirm Password" title="Enter password here to confirm your new password" tintColor='gray'></TextField>
+      <TouchableOpacity style={style.getOtpButton} onPress={() => this.onClickListener()}>
+            <Text style={style.getOtpText}>GET OTP</Text>
+      </TouchableOpacity>
       </View>
     );
   }
@@ -26,5 +35,20 @@ const style = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
     backgroundColor: '#F2F2F2',
+    padding: 20
+  },
+  textInput: {
+    height: 50
+  },
+  getOtpText: {
+    textAlign: 'center', fontSize: 22, color: 'white'
+  },
+  getOtpButton: {
+    backgroundColor: "#344756",
+    top: 70,
+    borderRadius: 5,
+    justifyContent: 'center',
+    width: '80%',
+    height: 50
   },
 })
